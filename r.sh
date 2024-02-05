a8cc3f8ce2cb61b5ee484bdaed2d642276b6ff3c
wilpeers <wilpeers@gmail.com>
2024-02-05 15:33:22 +0200

    basename?

--- r.sh
+++ r.sh
@@ -7,7 +7,7 @@ fi
 for f in ./*.py; do
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 	echo "Running $f >>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-    python3 "$f" | tee "__result/$(date +"%Y_%m_%d-%H-%M-%S")$f.txt"
+    python3 "$f" | tee "__result/$(date +"%Y_%m_%d-%H-%M-%S")$(basename '$f' )$f.txt"
     
     if [ $? -eq 0 ]; then
         echo "<<<<<<<<<<<<<<<<<<<<<< finished $f"
